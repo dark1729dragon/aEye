c2e31fec1117cca0028dce8661c1a9efe7a47975
dark1729dragon <32437027+dark1729dragon@users.noreply.github.com>
2018-12-24 22:05:57 +0530

    Update dataAugmentation.py
    implemented batch option 
    implemented small optimization
    implemented randomization

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -3,45 +3,51 @@ This is an example for data augmentation where
  input will be list of image paths (not image)
  output will be yield function with augmented image
 '''
-from glob import glob
 
+from glob import glob
 import numpy as np
 import cv2
-np.random.seed(42)
 
 
-def readImages(impaths):
-    imgScaleFactors = [.5,.25,1.5,2]
-    imgRotationFactors = [cv2.ROTATE_90_CLOCKWISE,cv2.ROTATE_90_COUNTERCLOCKWISE,cv2.ROTATE_180]
-    for impath in impaths:
-        img = cv2.imread(impath)
+def genAugmentation(img, imgScaleFactor, rotationFactor):
+    # scale the image
+    rows, cols = img.shape[:2]
+    rows, cols = rows * imgScaleFactor, cols * imgScaleFactor
+    rows, cols = int(rows), int(cols)
+    resizedImg = cv2.resize(img, (cols, rows))
+    # rotate image
+    # M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 90, 1)
+    # yield cv2.warpAffine(img, M, (cols, rows))
+    resizedRotatedImg = cv2.rotate(resizedImg, rotationFactor)
+    return resizedRotatedImg
+
+
+def readImages(impaths, batchSize, nGenImages):
+    imgScaleFactors = [.5, .25, 1, 1.5, 2]
+    imgRotationFactors = [cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_90_COUNTERCLOCKWISE, cv2.ROTATE_180]
+    imgs = []
+    while nGenImages:
+        img = cv2.imread(np.random.choice(impaths))
         if img is None:
             # img is empty then go for next image
             pass
         else:
-            yield img
-            # scale the image
-            for imgScaleFactor in imgScaleFactors:
-                rows, cols = img.shape[:2]
-                rows, cols = rows * imgScaleFactor, cols * imgScaleFactor
-                rows, cols = int(rows), int(cols)
-                resizedImg = cv2.resize(img, (cols, rows))
-                yield resizedImg
-                # rotate image
-                # M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 90, 1)
-                # yield cv2.warpAffine(img, M, (cols, rows))
-                for rotationFactor in imgRotationFactors:
-                    resizedRotatedImg = cv2.rotate(resizedImg, rotationFactor)
-                    yield resizedRotatedImg
+            imgs.append(genAugmentation(img, np.random.choice(imgScaleFactors), np.random.choice(imgRotationFactors)))
+        if len(imgs) == batchSize:
+            yield imgs
+            nGenImages -= 1
+            imgs = [] # empty all imgs
 
 
 def main(impaths):
-    for img in readImages(impaths):
-        cv2.namedWindow('img', 0)
-        cv2.imshow('img', img)
+    for imgs in readImages(impaths, 3, nGenImages=50):
+        for ix, img in enumerate(imgs):
+            cv2.namedWindow('img_%s' % ix, 0)
+            cv2.imshow('img_%s' % ix, img)
         cv2.waitKey(0)
 
 
 if __name__ == '__main__':
     impaths = glob(r'C:\Users\Public\Pictures\Sample Pictures\*.*')
     main(impaths)
+
